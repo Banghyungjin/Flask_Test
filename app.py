619b2f0ed29f278d27b3bda74429dc05bd4e6dbd
HJBang <banghyungjin@gmail.com>
2021-04-19 14:44:41 +0900

    2021-04-19_04

--- app.py
+++ app.py
@@ -5,21 +5,22 @@ app = Flask(__name__)
 
 app.debug = True
 
-@app.route('/data', methods = ['GET'])
+@app.route('/', methods = ['GET'])
 def index():
     # return "Hello World !"
     return render_template("index.html", data = "Bang")
 
 @app.route('/about')
 def about():
-    return render_template("about.html", hello = "H J Bang")
+    return render_template("about.html")
+
+@app.route('/articles')
+def articles():
+    return render_template("articles.html")
 
 @app.route('/Question')
 def Qeustion():
     return render_template("Question.html")
-
-    
-
 
 
 if __name__ == '__main__':
