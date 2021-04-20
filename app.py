6f73c61da708b2621207aacba7b4778576409013
HJBang <banghyungjin@gmail.com>
2021-04-20 17:06:45 +0900

    2021-04-20_01

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from data import Articles
-
+import pymysql
 
 app = Flask(__name__)
 
 app.debug = True
+
+db = pymysql.connect(
+    host = 'localhost',
+    port = 3306,
+    user = 'root',
+    password = '1234',
+    db = 'busan'
+)
+
+
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -17,21 +27,62 @@ def about():
 
 @app.route('/articles')
 def articles():
-    articles = Articles()
+    db = pymysql.connect(
+        host = 'localhost',
+        port = 3306,
+        user = 'root',
+        password = '1234',
+        db = 'busan'
+    )
+    cursor = db.cursor()
+    sql = 'SELECT * FROM topic'
+    cursor.execute(sql)
+    topics = cursor.fetchall()
+    #articles = Articles()
     #print(articles[0]['title'])
-    return render_template("articles.html", articles = articles)
+    return render_template("articles.html", articles = topics)
 
 @app.route('/article/<int:id>') #<id> 를 params 라고 해서 메소드에서 써먹을 수 있다.
 def article(id):
-    articles = Articles()
-    article = articles[id - 1]
-    return render_template("article.html", article = article)
+    cursor = db.cursor()
+    #articles = Articles()
+    #article = articles[id - 1]
+    sql = 'SELECT * FROM topic WHERE id = {};'.format(id)
+    cursor.execute(sql)
+    topic = cursor.fetchone()
+    return render_template("article.html", article = topic)
+
+@app.route('/add_articles', methods = ["GET", "POST"])
+def add_articles():
+    cursor = db.cursor()
+    if request.method == "POST":
+        desc = request.form['Desc']
+        title = request.form['Title']
+        author = request.form['Author']
+        sql_insert = "INSERT INTO `busan`.`topic` (`title`, `body`, `author`) VALUES (%s, %s, %s);" 
+        val = [title, desc, author]
+        cursor.execute(sql_insert, val)
+        db.commit()
+        topic = cursor.fetchall()
+        #db.close()
+        return redirect("/articles")
+    else:
+        return render_template("add_articles.html")
+
+@app.route('/delete/<int:id>', methods = ["POST"])
+def delete(id):
+    cursor = db.cursor()
+    sql_insert = "DELETE FROM topic WHERE id = {};".format(id) 
     
+    cursor.execute(sql_insert)
+    db.commit()
+    topic = cursor.fetchall()
+    #db.close()
+    return redirect("/articles")
 
 @app.route('/Question')
 def Qeustion():
     return render_template("Question.html")
-
 
 if __name__ == '__main__':
     app.run()
---
+++ dbmake.py
@@ -0,0 +1,38 @@
+import pymysql
+
+db = pymysql.connect(
+    host = 'localhost',
+    port = 3306,
+    user = 'root',
+    password = '1234',
+    db = 'busan'
+)
+
+sql = '''
+    CREATE TABLE `topic` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`title` varchar(100) NOT NULL,
+	`body` text NOT NULL,
+	`author` varchar(30) NOT NULL,
+    `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+	) ENGINE=innoDB DEFAULT CHARSET=utf8;
+'''
+
+sql_insert = "INSERT INTO `busan`.`users` (`title`, `body`, `author`) VALUES ('방형진', 'banghyungjin@gmail.com', 'Bang','12345');"
+
+title = input('제목을 적으세요 = ')
+body = input('내용을 적으세요 = ')
+author = input('저자를 적으세요 = ')
+
+sql_insert_2 = "INSERT INTO `busan`.`topic` (`title`, `body`, `author`) VALUES (%s, %s, %s);" 
+val = [title, body, author]
+
+cursor = db.cursor()
+cursor.execute(sql_insert_2, val)
+print(cursor.rowcount, "개의 줄이 추가되었습니다.")
+db.commit()
+db.close()
+#cursor.execute('SELECT * FROM topic;')
+#users = cursor.fetchall()
+#print(users)
