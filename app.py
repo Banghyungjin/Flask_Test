8a3f8372eaa4cb9157fb9fc1bb5e0eba66613711
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-04-22 17:47:19 +0900

    2021-04-22_01

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, flash, session
-from data import Articles
+from flask import Flask, render_template, request, redirect, flash, session, send_file
 from passlib.hash import sha256_crypt
 import pymysql
+import matplotlib.pyplot as plt
+import numpy as np
+from io import BytesIO, StringIO    #그래프를 이미지로 저장하기위한 변환 라이브러리
 
 app = Flask(__name__)
 
@@ -10,26 +12,27 @@ app.secret_key = 'my_secret_key'
 app.debug = True
 # db 로그인
 db = pymysql.connect(
-    host = 'localhost',
-    port = 3306,
-    user = 'root',
-    password = '1234',
-    db = 'busan'
+    host='localhost',
+    port=3306,
+    user='root',
+    password='1234',
+    db='busan'
 )
 
-#메인 화면& 로그인
-@app.route('/', methods = ['GET', 'POST'])
+
+# 메인 화면& 로그인
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # return "Hello World !"
     if session.get('is_logged') is not None:
-        return render_template("index.html", user = session.get('is_logged'))
+        return render_template("index.html", user=session.get('is_logged'))
     elif request.method == "POST":
         db = pymysql.connect(
-            host = 'localhost',
-            port = 3306,
-            user = 'root',
-            password = '1234',
-            db = 'busan'
+            host='localhost',
+            port=3306,
+            user='root',
+            password='1234',
+            db='busan'
         )
         cursor = db.cursor()
         sql = 'SELECT password FROM users where username = %s;'
@@ -37,16 +40,18 @@ def index():
         ps = request.form['psword']
         cursor.execute(sql, usid)
         user = cursor.fetchone()
-        if user == None:
+        if user is None:
             flash("없는 아이디입니다.")
         elif sha256_crypt.verify(ps, user[0]):
             session['is_logged'] = usid
-            return render_template("index.html", user = session.get('is_logged'))
+            return render_template("index.html", user=session.get('is_logged'))
         else:
             flash("틀린 비밀번호입니다.")
         return render_template("log_in.html")
     else:
         return render_template("log_in.html")
+
+
 # 정보화면 - 아직 만드는 중
 @app.route('/about')
 def about():
@@ -55,77 +60,88 @@ def about():
     else:
         return render_template("log_in.html")
 
+
 # 로그아웃 기능
 @app.route('/log_out')
 def log_out():
     session.clear()
     return render_template("log_in.html")
-# db에서 목록 읽어오는 기능
-@app.route('/articles', methods = ["GET", "POST"])
+
+
+# db 목록 읽어오는 기능
+@app.route('/articles', methods=["GET", "POST"])
 def articles():
     if session.get('is_logged') is not None:
         db = pymysql.connect(
-            host = 'localhost',
-            port = 3306,
-            user = 'root',
-            password = '1234',
-            db = 'busan'
+            host='localhost',
+            port=3306,
+            user='root',
+            password='1234',
+            db='busan'
         )
         cursor = db.cursor()
         sql = 'SELECT * FROM topic'
         cursor.execute(sql)
         topics = cursor.fetchall()
-        #articles = Articles()
-        #print(articles[0]['title'])
-        return render_template("articles.html", articles = topics)
+        # articles = Articles()
+        # print(articles[0]['title'])
+        return render_template("articles.html", articles=topics)
     else:
         return render_template("log_in.html")
-# articles에서 선택한 걸 자세히 표시하는 기능
-@app.route('/article/<int:id>') #<id> 를 params 라고 해서 메소드에서 써먹을 수 있다.
+
+
+# articles 선택한 걸 자세히 표시하는 기능
+@app.route('/article/<int:id>')  # <id> 를 params 라고 해서 메소드에서 써먹을 수 있다.
 def article(id):
     cursor = db.cursor()
-    #articles = Articles()
-    #article = articles[id - 1]
+    # articles = Articles()
+    # article = articles[id - 1]
     sql = 'SELECT * FROM topic WHERE id = {};'.format(id)
     cursor.execute(sql)
     topic = cursor.fetchone()
-    return render_template("article.html", article = topic)
-# 새로운 article을 추가하는 기능
-@app.route('/add_articles', methods = ["GET", "POST"])
+    return render_template("article.html", article=topic)
+
+
+# 새로운 article 추가하는 기능
+@app.route('/add_articles', methods=["GET", "POST"])
 def add_articles():
     cursor = db.cursor()
     if request.method == "POST":
         desc = request.form['Desc']
         title = request.form['Title']
         author = request.form['Author']
-        sql_insert = "INSERT INTO `busan`.`topic` (`title`, `body`, `author`) VALUES (%s, %s, %s);" 
+        sql_insert = "INSERT INTO `busan`.`topic` (`title`, `body`, `author`) VALUES (%s, %s, %s);"
         val = [title, desc, author]
         cursor.execute(sql_insert, val)
         db.commit()
         topic = cursor.fetchall()
-        #db.close()
+        # db.close()
         return redirect("/articles")
     else:
         return render_template("add_articles.html")
+
+
 # article을 제거하는 기능
-@app.route('/delete/<int:id>', methods = ["POST"])
+@app.route('/delete/<int:id>', methods=["POST"])
 def delete(id):
     cursor = db.cursor()
-    sql_insert = "DELETE FROM topic WHERE id = {};".format(id) 
+    sql_insert = "DELETE FROM topic WHERE id = {};".format(id)
     cursor.execute(sql_insert)
     db.commit()
     topic = cursor.fetchall()
-    #db.close()
+    # db.close()
     return redirect("/articles")
+
+
 # article을 수정하는 기능
-@app.route('/change_articles/<int:id>', methods = ["GET", "POST"])
+@app.route('/change_articles/<int:id>', methods=["GET", "POST"])
 def change_articles(id):
     cursor = db.cursor()
     if request.method == "POST":
         desc = request.form['Desc']
         title = request.form['Title']
         author = request.form['Author']
-        sql_change = "UPDATE topic SET title = %s, body = %s, author = %s, create_date = NOW() WHERE (id = %s);" 
+        sql_change = "UPDATE topic SET title = %s, body = %s, author = %s, create_date = NOW() WHERE (id = %s);"
         val = [title, desc, author, id]
         cursor.execute(sql_change, val)
         db.commit()
@@ -136,9 +152,11 @@ def change_articles(id):
     sql = 'SELECT * FROM topic WHERE id = {};'.format(id)
     cursor.execute(sql)
     topic = cursor.fetchone()
-    return render_template("change_articles.html", article = topic)
+    return render_template("change_articles.html", article=topic)
+
+
 # 회원가입 기능
-@app.route('/register', methods = ["GET", "POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
     cursor = db.cursor()
     if request.method == "POST":
@@ -146,15 +164,44 @@ def register():
         email = request.form['Email']
         username = request.form['Username']
         psword = sha256_crypt.encrypt(request.form['psword'])
-        sql_insert = "INSERT INTO `busan`.`users` (`name`, `email`, `username` , `password`) VALUES (%s, %s, %s, %s);" 
+        sql_insert = "INSERT INTO `busan`.`users` (`name`, `email`, `username` , `password`) VALUES (%s, %s, %s, %s);"
         val = [name, email, username, psword]
         cursor.execute(sql_insert, val)
         db.commit()
         topic = cursor.fetchall()
-        #db.close()
+        # db.close()
         return redirect("/")
     else:
         return render_template("register.html")
+
+
+@app.route('/fig/<int:mean>_<int:var>')
+def fig(mean, var):
+    if session.get('is_logged') is not None:
+        plt.figure(figsize = (4,3))
+        xs = np.random.normal(mean, var, 100)
+        ys = np.random.normal(mean, var, 100)
+        plt.scatter(xs,ys, s= 100, marker = 'h', color ='red', alpha = 0.3)
+        img = BytesIO()
+        plt.savefig(img, format = 'png', dpi = 200)
+        img.seek(0)
+        print(xs)
+        return send_file(img, mimetype = 'image/png')
+    else:
+        return render_template("log_in.html")
+
+@app.route('/graphes/')
+def graphes():
+    if session.get('is_logged') is not None:
+        # m, v = m_v.split('_')
+        # m, v = int(m), int(v)
+        m, v = 3, 5
+        return render_template("graphes.html", mean = m, var = v, width = 1000, height = 1000)
+    else:
+        return render_template("log_in.html")
+
+
+
 
 # @app.route('/Question')
 # def Qeustion():
